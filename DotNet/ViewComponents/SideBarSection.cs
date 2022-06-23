862132cf4a9c1a65e5386e67c69e5f24272ae6e6
Iqra Sarwar <71145709+iqrasarwar@users.noreply.github.com>
2022-06-23 22:26:21 +0500

    view components added

---
+++ DotNet/ViewComponents/SideBarSection.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+namespace LoginUsingIdentity.ViewComponents
+{
+    public class SideBarSection : ViewComponent
+    {
+        public IViewComponentResult Invoke(string SectionString)
+        {
+            
+            if(SectionString == "first section")
+            {
+                return View("NavBarPartial","rendered by view component");
+            }
+            else
+            {
+                return View("Default");
+            }
+        }
+    }
+}
